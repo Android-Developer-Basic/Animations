4cd985ac86a4fdcca0c8e9513d71884beb5b145e
Ponyu-dev <mopoypa@yandex.ru>
2024-01-26 07:35:29 +0300

    для анимации Tiktok style Loading Progress вынес некоторые данные в атрибуты которые можно настроить через разметку

--- app/src/main/java/ru/otus/animations/LoadingAnimationView.kt
+++ app/src/main/java/ru/otus/animations/LoadingAnimationView.kt
@@ -11,23 +11,22 @@ import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
 
-class LoadingAnimationView @JvmOverloads constructor(
-    context: Context? = null,
-    attrs: AttributeSet? = null,
-    defStyleAttr: Int = 0,
-    defStyleRes: Int = 0
-): View(context, attrs, defStyleAttr, defStyleRes) {
-
-    private val margin = 100
+class LoadingAnimationView : View {
+    private var autoStartOfAnimation = DEFAULT_AUTO_START_OF_ANIMATION;
+    private var spaceBetween = DEFAULT_SPACE_BETWEEN
+    private var durationAnimation: Long = DEFAULT_DURATION_ANIMATION.toLong()
+    private var delayAnimation: Long = DEFAULT_START_DELAY_ANIMATION.toLong()
+    private var leftCircleColor = DEFAULT_LEFT_CIRCLE_COLOR.toInt()
+    private var rightCircleColor = DEFAULT_RIGHT_CIRCLE_COLOR.toInt()
 
     private val centerX get() = width / 2f;
     private val centerY get() = height / 2f
-    private val radius get() = minOf(width, height) / 4f - margin
+    private val radius get() = minOf(width, height) / 4f - spaceBetween
 
-    private val leftCircleOriginalPositionX: Float get() = centerX - radius - (margin / 2)
+    private val leftCircleOriginalPositionX: Float get() = centerX - radius - (spaceBetween / 2)
     private var leftDx = 0f
     private var leftScale = 1f
-    private val rightCircleOriginalPositionX: Float get() = centerX + radius + (margin / 2)
+    private val rightCircleOriginalPositionX: Float get() = centerX + radius + (spaceBetween / 2)
     private var rightDx = 0f
     private var rightScale = 1f
     private var rightAlpha = 255
@@ -35,11 +34,64 @@ class LoadingAnimationView @JvmOverloads constructor(
     private val paint = Paint()
     private var isRunningAnimation = false
 
+    constructor(context: Context?) : super(context)
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
+        init(context, attrs)
+    }
+
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    ) {
+        init(context, attrs)
+    }
+
+    private fun init(context: Context, attrs: AttributeSet?) {
+        if (isInEditMode) return
+        requireNotNull(attrs) { "Attributes should be provided to this view," }
+
+        val typedArray = context.obtainStyledAttributes(attrs, R.styleable.LoadingAnimationView)
+
+        autoStartOfAnimation = typedArray.getBoolean(
+            R.styleable.LoadingAnimationView_autoStartOfAnimation,
+            DEFAULT_AUTO_START_OF_ANIMATION
+        )
+
+        leftCircleColor = typedArray.getColor(
+            R.styleable.LoadingAnimationView_leftCircleColor,
+            DEFAULT_LEFT_CIRCLE_COLOR.toInt()
+        )
+
+        rightCircleColor = typedArray.getColor(
+            R.styleable.LoadingAnimationView_rightCircleColor,
+            DEFAULT_RIGHT_CIRCLE_COLOR.toInt()
+        )
+
+        spaceBetween = typedArray.getInteger(
+            R.styleable.LoadingAnimationView_spaceBetween,
+            DEFAULT_SPACE_BETWEEN
+        )
+
+        durationAnimation = typedArray.getInt(
+            R.styleable.LoadingAnimationView_durationAnimation,
+            DEFAULT_DURATION_ANIMATION
+        ).toLong()
+
+        delayAnimation = typedArray.getInt(
+            R.styleable.LoadingAnimationView_startDelayAnimation,
+            DEFAULT_START_DELAY_ANIMATION
+        ).toLong()
+    }
+
     override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
         super.onLayout(changed, left, top, right, bottom)
 
         leftDx = leftCircleOriginalPositionX
         rightDx = rightCircleOriginalPositionX
+
+        if (autoStartOfAnimation)
+            startFirstAnimation()
     }
 
     fun startFirstAnimation(){
@@ -48,8 +100,8 @@ class LoadingAnimationView @JvmOverloads constructor(
         isRunningAnimation = true
         val animatorSet = AnimatorSet()
         animatorSet.apply {
-            duration = 2000
-            startDelay = 200
+            duration = durationAnimation
+            startDelay = delayAnimation
 
             playTogether(
                 ValueAnimator.ofFloat(leftCircleOriginalPositionX, rightCircleOriginalPositionX).apply {
@@ -109,8 +161,8 @@ class LoadingAnimationView @JvmOverloads constructor(
         val animatorSet = AnimatorSet()
 
         animatorSet.apply {
-            duration = 2000
-            startDelay = 200
+            duration = durationAnimation
+            startDelay = delayAnimation
 
             playTogether(
                 ValueAnimator.ofFloat(rightCircleOriginalPositionX, leftCircleOriginalPositionX).apply {
@@ -151,11 +203,20 @@ class LoadingAnimationView @JvmOverloads constructor(
         super.onDraw(canvas)
 
         canvas?.drawCircle(leftDx, centerY, radius * leftScale, paint.apply {
-                color = Color.parseColor("#7c4fff")
+                color = leftCircleColor
             })
         canvas?.drawCircle(rightDx, centerY, radius * rightScale, paint.apply {
-                color = Color.parseColor("#ff39d3")
+                color = rightCircleColor
                 alpha = rightAlpha
             })
+    }
+
+    companion object {
+        private const val DEFAULT_AUTO_START_OF_ANIMATION = false
+        private const val DEFAULT_LEFT_CIRCLE_COLOR: Long = 0xFF7c4fff
+        private const val DEFAULT_RIGHT_CIRCLE_COLOR: Long = 0xFFff39d3
+        private const val DEFAULT_SPACE_BETWEEN = 100
+        private const val DEFAULT_DURATION_ANIMATION = 1000
+        private const val DEFAULT_START_DELAY_ANIMATION = 0
     }
 }
